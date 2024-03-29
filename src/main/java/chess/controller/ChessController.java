c48d00950e879ea25776795710f69da5f4baf000
J-I-H-O <jeongjiho0731@gmail.com>
2024-03-29 22:24:39 +0900

    refactor: 콘솔 입력 처리 객체 분리 및 반복 입력 메서드 간소화
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/chess/controller/ChessController.java
+++ src/main/java/chess/controller/ChessController.java
@@ -4,17 +4,13 @@ import chess.domain.ChessBoard;
 import chess.domain.ChessBoardFactory;
 import chess.domain.Position;
 import chess.domain.piece.Color;
+import chess.view.CommandArguments;
 import chess.view.GameCommand;
 import chess.view.InputView;
 import chess.view.OutputView;
-import java.util.List;
 import java.util.function.Supplier;
 
 public class ChessController {
-
-    private static final int COMMAND_INDEX = 0;
-    private static final int SOURCE_POSITION_INDEX = 1;
-    private static final int TARGET_POSITION_INDEX = 2;
 
     private final InputView inputView;
     private final OutputView outputView;
@@ -25,82 +21,76 @@ public class ChessController {
     }
 
     public void run() {
-        final ChessBoard chessBoard = ChessBoardFactory.makeChessBoard();
+        ChessBoard chessBoard = ChessBoardFactory.makeChessBoard();
         outputView.printCommandInformation();
+        CommandArguments commandArguments = repeatUntilSuccess(() -> readCommandBeforeGame());
+        GameCommand gameCommand = commandArguments.parseCommand();
 
-        List<String> commandArguments = repeat(this::readInitialCommand);
-        String gameCommand = commandArguments.get(COMMAND_INDEX);
-        outputView.printChessBoard(chessBoard);
-
-        repeat(() -> playTurn(chessBoard, gameCommand, commandArguments));
-    }
-
-    private void playTurn(ChessBoard chessBoard, String gameCommand, List<String> commandArguments) {
-        // TODO: 현재 King이 죽으면 바로 게임이 끝나지 않고 추가 명령어를 입력해야 하는 상황. 수정 필요.
-        while (!GameCommand.isEndCommand(gameCommand) && !chessBoard.isKingDead()) {
-            if (GameCommand.isMoveCommand(gameCommand)) {
-                playMoveCommand(chessBoard, gameCommand, commandArguments);
-                outputView.printChessBoard(chessBoard);
-            }
-            if (GameCommand.isStatusCommand(gameCommand)) {
-                double blackScore = chessBoard.calculateScoreByColor('a', 'h', 1, 8, Color.BLACK);
-                double whiteScore = chessBoard.calculateScoreByColor('a', 'h', 1, 8, Color.WHITE);
-                outputView.printScoreStatus(blackScore, whiteScore);
-            }
-
-            commandArguments = readInGameCommand();
-            gameCommand = commandArguments.get(COMMAND_INDEX);
-        }
-    }
-
-    private void playMoveCommand(final ChessBoard chessBoard,
-                                 final String gameCommand,
-                                 final List<String> commandArguments) {
-        if (GameCommand.isMoveCommand(gameCommand)) {
-            Position source = Position.from(commandArguments.get(SOURCE_POSITION_INDEX));
-            Position target = Position.from(commandArguments.get(TARGET_POSITION_INDEX));
-            chessBoard.move(source, target);
+        while (gameCommand != GameCommand.END && !chessBoard.isKingDead()) {
             outputView.printChessBoard(chessBoard);
+            commandArguments = repeatUntilSuccess(() -> readAndExecuteCommandDuringGame(chessBoard));
+            gameCommand = commandArguments.parseCommand();
         }
     }
 
-    private List<String> readInitialCommand() {
-        List<String> commandArguments = inputView.readGameCommand();
-        String gameCommand = commandArguments.get(COMMAND_INDEX);
+    private CommandArguments readCommandBeforeGame() {
+        CommandArguments commandArguments = inputView.readGameCommand();
+        GameCommand gameCommand = commandArguments.parseCommand();
+        validateCommandBeforeGame(gameCommand);
 
-        if (GameCommand.isMoveCommand(gameCommand) || GameCommand.isStatusCommand(gameCommand)) {
+        return commandArguments;
+    }
+
+    private CommandArguments readAndExecuteCommandDuringGame(final ChessBoard chessBoard) {
+        CommandArguments commandArguments = inputView.readGameCommand();
+        GameCommand gameCommand = commandArguments.parseCommand();
+        validateCommandDuringGame(gameCommand);
+        executeCommand(gameCommand, commandArguments, chessBoard);
+
+        return commandArguments;
+    }
+
+    private void executeCommand(final GameCommand gameCommand,
+                                final CommandArguments commandArguments,
+                                final ChessBoard chessBoard) {
+        if (gameCommand == GameCommand.MOVE) {
+            executeMoveCommand(commandArguments, chessBoard);
+        }
+        if (gameCommand == GameCommand.STATUS) {
+            executeStatusCommand(chessBoard);
+        }
+    }
+
+    private void executeMoveCommand(final CommandArguments commandArguments, final ChessBoard chessBoard) {
+        Position sourcePosition = Position.from(commandArguments.getFirstArgument());
+        Position targetPosition = Position.from(commandArguments.getSecondArgument());
+        chessBoard.move(sourcePosition, targetPosition);
+    }
+
+    private void executeStatusCommand(final ChessBoard chessBoard) {
+        double blackScore = chessBoard.calculateScoreByColor('a', 'h', 1, 8, Color.BLACK);
+        double whiteScore = chessBoard.calculateScoreByColor('a', 'h', 1, 8, Color.WHITE);
+        outputView.printScoreStatus(blackScore, whiteScore);
+    }
+
+    private void validateCommandBeforeGame(final GameCommand gameCommand) {
+        if (!gameCommand.canExecuteBeforeGame()) {
             throw new IllegalArgumentException("[ERROR] 먼저 게임을 시작해야 합니다.");
         }
-
-        return commandArguments;
     }
 
-    private List<String> readInGameCommand() {
-        List<String> commandArguments = inputView.readGameCommand();
-        String gameCommand = commandArguments.get(COMMAND_INDEX);
-
-        if (GameCommand.isStartCommand(gameCommand)) {
-            throw new IllegalArgumentException("[ERROR] 이미 게임이 시작된 상태입니다.");
-        }
-
-        return commandArguments;
-    }
-
-    private <T> T repeat(final Supplier<T> supplier) {
-        try {
-            return supplier.get();
-        } catch (IllegalArgumentException e) {
-            outputView.printErrorMessage(e.getMessage());
-            return repeat(supplier);
+    private void validateCommandDuringGame(final GameCommand gameCommand) {
+        if (!gameCommand.canExecuteDuringGame()) {
+            throw new IllegalArgumentException("[ERROR] 이미 게임이 실행중입니다.");
         }
     }
 
-    private void repeat(final Runnable runnable) {
+    private CommandArguments repeatUntilSuccess(final Supplier<CommandArguments> reader) {
         try {
-            runnable.run();
-        } catch (IllegalArgumentException e) {
+            return reader.get();
+        } catch (Exception e) {
             outputView.printErrorMessage(e.getMessage());
-            repeat(runnable);
+            return repeatUntilSuccess(reader);
         }
     }
 }
