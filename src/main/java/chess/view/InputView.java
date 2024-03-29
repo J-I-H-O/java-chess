e46be3e09091266bf43a38a4c08c307469580833
J-I-H-O <jeongjiho0731@gmail.com>
2024-03-29 22:24:39 +0900

    refactor: 콘솔 입력 처리 객체 분리 및 반복 입력 메서드 간소화
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/chess/view/InputView.java
+++ src/main/java/chess/view/InputView.java
@@ -1,26 +1,13 @@
 package chess.view;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
-    public static final String COMMAND_DELIMITER = " ";
-
     private final Scanner scanner = new Scanner(System.in);
 
-    public List<String> readGameCommand() {
+    public CommandArguments readGameCommand() {
         String rawInput = scanner.nextLine();
-        if (GameCommand.isStartCommand(rawInput) ||
-                GameCommand.isEndCommand(rawInput) ||
-                GameCommand.isStatusCommand(rawInput)) {
-            return List.of(rawInput);
-        }
-        if (GameCommand.isMovePattern(rawInput)) {
-            return Arrays.stream(rawInput.split(COMMAND_DELIMITER))
-                    .toList();
-        }
-        throw new IllegalArgumentException("[ERROR] 유효하지 않은 입력입니다.");
+        return new CommandArguments(rawInput);
     }
 }
