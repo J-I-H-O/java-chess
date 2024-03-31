6cde62d63fc53dcdaf9b2ec78ae4c4f20438ec08
J-I-H-O <jeongjiho0731@gmail.com>
2024-04-01 08:06:54 +0900

    refactor: 턴 관리를 위한 클래스 분리
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

---
+++ src/main/java/chess/domain/ChessGame.java
@@ -0,0 +1,34 @@
+package chess.domain;
+
+import chess.domain.piece.Color;
+
+public class ChessGame {
+
+    private final ChessBoard chessBoard;
+    private Color turnColor;
+
+    public ChessGame(final ChessBoard chessBoard, final Color turnColor) {
+        this.chessBoard = chessBoard;
+        this.turnColor = turnColor;
+    }
+
+    public void move(final Position source, final Position target) {
+        validateTurn(source, this.turnColor);
+        chessBoard.move(source, target);
+        this.turnColor = Color.switchColor(this.turnColor);
+    }
+
+    private void validateTurn(final Position source, final Color turnColor) {
+        if (!chessBoard.findPieceByPosition(source).isSameColorWith(turnColor)) {
+            throw new IllegalArgumentException(String.format("[ERROR] 지금은 %s의 턴입니다.", turnColor));
+        }
+    }
+
+    public double calculateScoreByColor(final ScoreCalculator scoreCalculator, final Color color) {
+        return scoreCalculator.calculateScore(chessBoard.filterPiecesByColor(color));
+    }
+
+    public boolean isGameOver() {
+        return chessBoard.isKingDead();
+    }
+}
