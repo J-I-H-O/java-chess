3323f7cfaf7a526b3ccb827fb8d77cb7d37d9936
J-I-H-O <jeongjiho0731@gmail.com>
2024-04-03 11:30:38 +0900

    refactor: DB 연결 설정 파일 분리
    Co-authored-by: J-I-H-O <jeongjiho0731@gmail.com>

--- src/main/java/chess/database/DbConnection.java
+++ src/main/java/chess/database/DbConnection.java
@@ -1,25 +1,33 @@
 package chess.database;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DbConnection {
 
-    public static final String SERVER = "localhost:13306";
-    public static final String DATABASE = "chess";
-    public static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "root";
+    private static final String CONFIG_FILE_PATH = "src/main/resources/db-config.yml";
 
     private DbConnection() {
     }
 
     public static Connection getConnection() {
         try {
-            String url = "jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION;
-            return DriverManager.getConnection(url, USERNAME, PASSWORD);
-        } catch (SQLException e) {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream(CONFIG_FILE_PATH));
+
+            String server = properties.getProperty("server");
+            String database = properties.getProperty("database");
+            String option = properties.getProperty("option");
+            String username = properties.getProperty("username");
+            String password = properties.getProperty("password");
+
+            String url = "jdbc:mysql://" + server + "/" + database + option;
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException | IOException e) {
             throw new IllegalStateException(e);
         }
     }
